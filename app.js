4812867f5b1d51d9180dc83b4686049a3897a34b
Haseeb Ullah Tarakai <haseebullah7aand7b@gmail.com>
2023-03-21 23:59:05 +0500

    local siginin and signout

--- app.js
+++ app.js
@@ -1,72 +1,76 @@
 //jshint esversion:6
-require('dotenv').config()
 const express = require('express');
 const app = express();
-const bcrypt=require('bcrypt');
-const saltRounds=10;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/secrets');
-const cors = require('cors');
+const session=require('express-session')
+const passport=require('passport')
+const localStrategy=require('passport-local').Strategy;
+const passportLocalMongoose=require('passport-local-mongoose')
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret:'My little secret',
+    resave:false,
+    saveUninitialized:false
+}
+));
+app.use(passport.initialize())
+app.use(passport.session())
+mongoose.connect('mongodb://127.0.0.1:27017/passport');
+
+
+
 const secretSchema=new mongoose.Schema({
-    email:String,
+    username:String,
     password:String
 })
-const secretModel=mongoose.model('secret',secretSchema)
-app.route('/')
-.get(function(req,res){
+secretSchema.plugin(passportLocalMongoose)
+
+const User=mongoose.model('User',secretSchema)
+passport.use(new localStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.get('/',function(req,res){
     res.render('home');
+}); 
+app.get('/secret',function(req,res){
+   if (req.isAuthenticated){
+    res.render('secrets')
+   }else{
+    res.render("login")
+   }
 });
-app.route('/login')
-.get(function(req,res){
+app.get('/register',function(req,res){
+    res.render('register');
+});
+app.post('/register',function(req,res){
+    const newUser=new User({username:req.body.username});
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secret')
+            })
+        }
+    })
+});
+app.get('/login',function(req,res){
     res.render('login');
     
 })
-.post(async function(req,res){
-    var users=[]
-    const emaila=req.body.email;
-        const loginAttempt=await secretModel.findOne({email:emaila})
-                bcrypt.compare(req.body.password,loginAttempt.password, function(err, result) {
-                     if(result == true){
-                        res.render('secrets',{message:'ok'});
-                    }else{
-                        console.log('err')
-                    }
-                });
-    // loginAttempt.forEach(function(user){
-    // users.push(user)
-    // })
-    // for(let i=0; i<users.length; i++){
-    //     if (users[i].email === email && users[i].password === password){
-    //         res.render('secrets',{message:'ok'});
-    //     }else{
-    //         console.log('data not found');
-    //     }
-    // }
-});
-app.route('/register')
-.get(function(req,res){
-    res.render('register');
+app.post('/login',passport.authenticate('local',{
+    successRedirect:'/secret',
+    failureRedirect:'/login'
+}))
+app.get('/logout',function(){
+    res.redirect('/')
 })
-.post(function(req,res){
-    const email=req.body.email;
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const loginAttempt=new secretModel({
-            email:email,
-            password:hash
-        })
-        
-        loginAttempt.save().then(()=>console.log('The data has been parsed to the server')).then(function(){
-            res.redirect('/');
-        })
-        
-    });
-    
-});
+
 
 app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 3000");
